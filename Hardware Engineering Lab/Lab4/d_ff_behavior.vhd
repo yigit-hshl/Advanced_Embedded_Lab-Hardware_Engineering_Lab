d202cd31924e8c945781c83cad210f507b0fd66e
Muhammad Rohail Usman <56233774+Rohail999@users.noreply.github.com>
2022-07-07 00:11:40 +0200

    Create d_ff_behavior.vhd

---
+++ Hardware Engineering Lab/Lab4/d_ff_behavior.vhd
@@ -0,0 +1,24 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity D_FLIPFLOP_SOURCE is
+Port ( D, CLK, RST : in  STD_LOGIC;
+Q, Qb : out  STD_LOGIC);
+end D_FLIPFLOP_SOURCE;
+
+architecture Behavioral of D_FLIPFLOP_SOURCE is
+
+begin
+  process (D, CLK, RST)
+begin
+  if (RST = '1') then
+Q <= '0';
+  elsif (rising_edge(CLK)) then
+Q <= D;
+Qb <= not D;
+  
+  end if;
+end process;
+end Behavioral;
